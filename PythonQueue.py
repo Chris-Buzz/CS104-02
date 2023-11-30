8ea537db2aae4fc688147e4b3bc1885282cf9313
Chris-Buzz <146122780+Chris-Buzz@users.noreply.github.com>
2023-11-30 00:19:57 -0500

    Python Queue Simulation

---
+++ PythonQueue.py
@@ -0,0 +1,15 @@
+names = []
+
+nameCount = 0
+for i in range(10):
+    acceptedName = input("Enter name: ")
+    names.append(acceptedName)
+    nameCount +=1
+
+
+print(names)
+
+for i in range(10):
+    len(names)
+    print(names.pop(0))
+    
